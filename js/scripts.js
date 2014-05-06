f0c8de79ec9206b4525cee93b46f9295afbf9dd3
Gokou Ruri <a2798055@drdrb.net>
2014-05-07 00:09:55 +0200

    Fix 2D girl escape

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 $(window).resize(function(event) {
-    if ( $(window).width() <= 750 && $('main').hasClass('waifu') ){
-        $('main').removeClass('waifu');
+    if ( $(window).width() <= 750 && $('main').hasClass(waifu) ){
+        $('main').removeClass(waifu);
         $('main').addClass('plain');
     } else if( $(window).width() > 750 && $('main').hasClass('plain') ) {
-        $('main').addClass('waifu');
+        $('main').addClass(waifu);
         $('main').removeClass('plain');
     }
 });
 
 $(document).ready(function(event) {
     var waifus = [ 'waifu1', 'waifu2', 'waifu3' ];
-    var waifu = waifus[Math.floor(Math.random() * waifus.length)];
+    waifu = waifus[Math.floor(Math.random() * waifus.length)];
     $('main').addClass(waifu);
 });
