553f850b7cbb6632fc0f3beb91db2d2c97c841b1
Gokou Ruri <a2798055@drdrb.net>
2014-05-08 20:03:13 +0200

    Cleanup javascript. Change all instances of the word 'waifu' to 'mascot'. This is serious business after all.

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,25 @@
     This work is free. You can redistribute it and/or modify it under the
     terms of the Do What The Fuck You Want To Public License, Version 2,
     as published by Sam Hocevar. See the COPYING file for more details. */
-$(window).resize(function(event) {
-    if ( $(window).width() <= 750 && $('main').hasClass(waifu) ){
-        $('main').removeClass(waifu);
-        $('main').addClass('plain');
-    } else if( $(window).width() > 750 && $('main').hasClass('plain') ) {
-        $('main').addClass(waifu);
-        $('main').removeClass('plain');
-    }
-});
+function controlMascot(mascot) {
+    $(window).resize(function(event) {
+        if ( $(window).width() <= 750 && $('main').hasClass(mascot) ){
+            $('main').removeClass(mascot);
+            $('main').addClass('plain');
+        } else if( $(window).width() > 750 && $('main').hasClass('plain') ) {
+            $('main').addClass(mascot);
+            $('main').removeClass('plain');
+        }
+    });
+}
+
+function setMascot(mascot) {
+    $('main').addClass(mascot);
+}
 
 $(document).ready(function(event) {
-    var waifus = [ 'waifu1', 'waifu2', 'waifu3' ];
-    waifu = waifus[Math.floor(Math.random() * waifus.length)];
-    $('main').addClass(waifu);
+    var mascotList = [ 'mascot1', 'mascot2', 'mascot3' ];
+    var mascot = mascotList[Math.floor(Math.random() * mascotList.length)];
+    setMascot(mascot);
+    controlMascot(mascot);
 });
