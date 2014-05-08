7cf84b2a05ef5a6f2f9040a09c8d9e99d9c0113b
Gokou Ruri <a2798055@drdrb.net>
2014-05-08 21:02:07 +0200

    Add js function to get all .mascotN classes from stylesheet. No need to manually modify an array in js anymore.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,14 @@
     This work is free. You can redistribute it and/or modify it under the
     terms of the Do What The Fuck You Want To Public License, Version 2,
     as published by Sam Hocevar. See the COPYING file for more details. */
-function controlMascot(mascot) {
+
+/*  Remove mascot class from element "main" if its width is <= mascotMinWidth */
+function controlMascot(mascot, mascotMinWidth) {
     $(window).resize(function(event) {
-        if ( $(window).width() <= 750 && $('main').hasClass(mascot) ){
+        if ( $(window).width() <= mascotMinWidth && $('main').hasClass(mascot) ){
             $('main').removeClass(mascot);
             $('main').addClass('plain');
-        } else if( $(window).width() > 750 && $('main').hasClass('plain') ) {
+        } else if( $(window).width() > mascotMinWidth && $('main').hasClass('plain') ) {
             $('main').addClass(mascot);
             $('main').removeClass('plain');
         }
@@ -18,9 +20,27 @@ function setMascot(mascot) {
     $('main').addClass(mascot);
 }
 
+/* Return all 'mascotN' classes defined in the styleSheet */
+function getMascotList() {
+    var mascotList  = [];
+    var rules       = document.styleSheets[0].rules || document.styleSheets[0].cssRules;
+
+    for (var i=0; i < rules.length; i++) {
+        var rule = rules[i];
+        var ruleName = rule.selectorText;
+        if ( ruleName == ruleName.match(/\.mascot\d\d?/) ) {
+            mascotList.push(ruleName.replace(".", ""))
+        }
+    }
+    
+    return mascotList;
+}
+
 $(document).ready(function(event) {
-    var mascotList = [ 'mascot1', 'mascot2', 'mascot3' ];
-    var mascot = mascotList[Math.floor(Math.random() * mascotList.length)];
+    var mascotList      = getMascotList();
+    var mascot          = mascotList[Math.floor(Math.random() * mascotList.length)];
+    var mascotMinWidth  = '750';
+
     setMascot(mascot);
-    controlMascot(mascot);
+    controlMascot(mascot, mascotMinWidth);
 });
