f765536129b9ca88728e35e92b019b47bf1d1047
Gokou Ruri <a2798055@drdrb.net>
2014-05-10 22:56:55 +0200

    Update pages

--- js/scripts.js
+++ js/scripts.js
@@ -6,41 +6,36 @@
 /*  Remove mascot class from element "main" if its width is <= mascotMinWidth */
 function controlMascot(mascot, mascotMinWidth) {
     $(window).resize(function(event) {
-        if ( $(window).width() <= mascotMinWidth && $('main').hasClass(mascot) ){
-            $('main').removeClass(mascot);
-            $('main').addClass('plain');
-        } else if( $(window).width() > mascotMinWidth && $('main').hasClass('plain') ) {
-            $('main').addClass(mascot);
-            $('main').removeClass('plain');
+        if ( $(window).width() <= mascotMinWidth && $("main").hasClass("mascot") ) {
+            removeMascot();
+        } else if( $(window).width() > mascotMinWidth && ! $("main").hasClass("mascot") ) {
+            setMascot(mascot);
         }
     });
 }
 
 function setMascot(mascot) {
-    $('main').addClass(mascot);
+    $('main').addClass("mascot");
+    $('main').css("background-image", "url(" + mascot + ")");
+    $('main').removeClass("plain");
 }
 
-/* Return all 'mascotN' classes defined in the styleSheet */
-function getMascotList() {
-    var mascotList  = [];
-    var rules       = document.styleSheets[0].rules || document.styleSheets[0].cssRules;
-
-    for (var i=0; i < rules.length; i++) {
-        var rule = rules[i];
-        var ruleName = rule.selectorText;
-        if ( ruleName == ruleName.match(/\.mascot\d\d?/) ) {
-            mascotList.push(ruleName.replace(".", ""))
-        }
-    }
-    
-    return mascotList;
+function removeMascot() {
+    $('main').removeClass("mascot");
+    $('main').css("background-image", "");
+    $('main').addClass("plain");
 }
 
 $(document).ready(function(event) {
-    var mascotList      = getMascotList();
-    var mascot          = mascotList[Math.floor(Math.random() * mascotList.length)];
+    var mascotEnable    = true;
+    var mascotPath      = "images/mascots/"
+    var mascotList      = [ 'ruri1.png', 'ruri2.png', 'ruri3.png' ];
+
+    var mascot          = mascotPath + mascotList[Math.floor(Math.random() * mascotList.length)];
     var mascotMinWidth  = '750';
 
-    setMascot(mascot);
-    controlMascot(mascot, mascotMinWidth);
+    if ( mascotEnable ) { 
+        setMascot(mascot);
+        controlMascot(mascot, mascotMinWidth);
+    } else { removeMascot(); }
 });
