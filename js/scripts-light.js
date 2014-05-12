394562fd409ec80c0498d1bcc14f69a176b624b7
Gokou Ruri <a2798055@drdrb.net>
2014-05-12 20:41:18 +0200

    Add light colortheme page

---
+++ js/scripts-light.js
@@ -0,0 +1,41 @@
+/*  Copyright 2014 gokoururi
+    This work is free. You can redistribute it and/or modify it under the
+    terms of the Do What The Fuck You Want To Public License, Version 2,
+    as published by Sam Hocevar. See the COPYING file for more details. */
+
+/*  Remove mascot class from element "main" if its width is <= mascotMinWidth */
+function controlMascot(mascot, mascotMinWidth) {
+    $(window).resize(function(event) {
+        if ( $(window).width() <= mascotMinWidth && $("main").hasClass("mascot") ) {
+            removeMascot();
+        } else if( $(window).width() > mascotMinWidth && ! $("main").hasClass("mascot") ) {
+            setMascot(mascot);
+        }
+    });
+}
+
+function setMascot(mascot) {
+    $('main').addClass("mascot");
+    $('main').css("background-image", "url(" + mascot + ")");
+    $('main').removeClass("plain");
+}
+
+function removeMascot() {
+    $('main').removeClass("mascot");
+    $('main').css("background-image", "");
+    $('main').addClass("plain");
+}
+
+$(document).ready(function(event) {
+    var mascotEnable    = true;
+    var mascotPath      = "images/mascots/"
+    var mascotList      = [ 'lightruri1.png', 'lightruri2.png', 'lightruri3.png' ];
+
+    var mascot          = mascotPath + mascotList[Math.floor(Math.random() * mascotList.length)];
+    var mascotMinWidth  = '750';
+
+    if ( mascotEnable ) { 
+        setMascot(mascot);
+        controlMascot(mascot, mascotMinWidth);
+    } else { removeMascot(); }
+});
