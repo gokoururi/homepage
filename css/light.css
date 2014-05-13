b39d8b80bba048c30f27d6853d097d5781759b07
Gokou Ruri <a2798055@drdrb.net>
2014-05-13 22:28:47 +0200

    Remove non utf8 character from light.css

--- css/light.css
+++ css/light.css
@@ -1,4 +1,4 @@
-/*  Copyright © 2014 gokoururi
+/*  Copyright 2014 gokoururi
     This work is free. You can redistribute it and/or modify it under the
     terms of the Do What The Fuck You Want To Public License, Version 2,
     as published by Sam Hocevar. See the COPYING file for more details. */
